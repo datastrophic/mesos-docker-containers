df3f0999c1f192bfb92742baa91bd455a1dbe28f
Anton Kirillov <anton.kirillov@ooyala.com>
2017-02-20 13:31:47 +0100

    entrypoint script update

--- mesos-spark-notebook/entrypoint.sh
+++ mesos-spark-notebook/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #This script is work in progress and will be changed in future
-
-mv ${SPARK_HOME}/conf/spark-env.sh.template ${SPARK_HOME}/conf/spark-env.sh
-
 for k in `env | grep ^SPARK_ | cut -d= -f1`; do
     eval v=\$$k
     CMD="$CMD `echo $k | cut -d_ -f2- | tr '[:upper:]' '[:lower:]' | tr '_' '.'`=$v"
 done
 
+echo "properties found:"
 echo $CMD
 
 if [ $# -gt 0 ]; then
