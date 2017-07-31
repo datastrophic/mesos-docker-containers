08cb27887fef9586710d89a226bb12384f949609
Anton Kirillov <anton.kirillov@ooyala.com>
2017-07-31 15:43:14 -0700

    minor fixes

--- apache-drill/entrypoint.sh
+++ apache-drill/entrypoint.sh
@@ -6,4 +6,4 @@ if [ $# -gt 0 ]; then
     exec "$@"
 fi
 
-${DRILL_HOME}/bin/drillbit.sh --config ${DRILL_HOME}/conf start && tail -f /apache-drill/log/drillbit.logdo
+${DRILL_HOME}/bin/drillbit.sh --config ${DRILL_HOME}/conf start && tail -f /apache-drill/log/drillbit.log
